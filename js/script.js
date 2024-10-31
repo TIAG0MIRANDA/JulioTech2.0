ebaca69cf0456fca9da427cbe2d35dd8db098190
Tiago Miranda <t.miranda0249@gmail.com>
2024-10-31 17:42:41 -0300

    header responsivo

--- js/script.js
+++ js/script.js
@@ -1,15 +1,60 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('.nav-link');
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const navLinks = document.querySelectorAll(".nav-link");
 
     navLinks.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault(); // Impede o comportamento padrão do link
+        link.addEventListener("click", function (event) {
 
             // Remove a classe 'active' de todos os links
-            navLinks.forEach(item => item.classList.remove('active'));
+            navLinks.forEach(item => item.classList.remove("active"));
 
             // Adiciona a classe 'active' ao link clicado
-            this.classList.add('active');
+            this.classList.add("active");
         });
     });
 });
+
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
